f82a769d71e2e90f1025db041ea54ce7dd57d231
Victor I. Afolabi <javafolabi@gmail.com>
2022-08-16 11:50:55 +0100

    Modified cuda and video capture

--- src/video/capture.rs
+++ src/video/capture.rs
@@ -1,32 +1,32 @@
 use opencv::{highgui, prelude::*, videoio, Result};
 
 pub fn video_capture() -> Result<()> {
-  let window = "Video capture";
-  highgui::named_window(window, highgui::WINDOW_AUTOSIZE)?;
+    let window = "Video capture";
+    highgui::named_window(window, highgui::WINDOW_AUTOSIZE)?;
 
-  #[cfg(ocvrs_opencv_branch_32)]
-  let mut cam = videio::VideoCapture::new_default(0)?;
-  #[cfg(not(ocvrs_opecv_branch_32))]
-  let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
+    #[cfg(ocvrs_opencv_branch_32)]
+    let mut cam = videio::VideoCapture::new_default(0)?;
+    #[cfg(not(ocvrs_opecv_branch_32))]
+    let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
 
-  let opened = videoio::VideoCapture::is_opened(&cam)?;
-  if !opened {
-    panic!("Unable to open default camera!");
-  }
-
-  // Video loop.
-  loop {
-    let mut frame = Mat::default();
-    cam.read(&mut frame)?;
-    if frame.size()?.width > 0 {
-      highgui::imshow(window, &mut frame)?;
+    let opened = videoio::VideoCapture::is_opened(&cam)?;
+    if !opened {
+        panic!("Unable to open default camera!");
     }
 
-    let key = highgui::wait_key(10)?;
-    if key > 0 && key != 255 {
-      break;
-    }
-  }
+    // Video loop.
+    loop {
+        let mut frame = Mat::default();
+        cam.read(&mut frame)?;
+        if frame.size()?.width > 0 {
+            highgui::imshow(window, &mut frame)?;
+        }
 
-  Ok(())
+        let key = highgui::wait_key(10)?;
+        if key > 0 && key != 255 {
+            break;
+        }
+    }
+
+    Ok(())
 }
